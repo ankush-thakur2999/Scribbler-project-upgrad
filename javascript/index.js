b3078582cf8e1410153a7775715731ca3278738c
ankush-thakur2999 <thakurankush2999@gmail.com>
2021-03-26 09:48:52 +0530

    added opacity filter

--- javascript/index.js
+++ javascript/index.js
@@ -1,20 +1,25 @@
 var signinbox = document.getElementById("Sign-in");
 var signupbox = document.getElementById("Sign-up");
+var opaq = document.getElementById("opaq");
 
 console.log("js");
 function signin(){
     console.log("si");
     signupbox.style.display="none";
     signinbox.style.display="block";
+    opaq.style.display="block";
 }
  function closesigninbox() {
      signinbox.style.display="none";
+     opaq.style.display="none";
  }
  function signup(){
   
     signinbox.style.display="none";
     signupbox.style.display="block";
+    opaq.style.display="block";
  }
  function closesignupbox() {
     signupbox.style.display="none";
+    opaq.style.display="none";
 }
